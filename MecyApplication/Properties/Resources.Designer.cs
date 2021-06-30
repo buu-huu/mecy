5844906aa78da7060fb8f34b6ca5e8c48391c0e7
buu-huu <luca-schwarz@outlook.de>
2021-07-01 01:19:46 +0200

    Add toolbar icon for distance measuring

--- MecyApplication/Properties/Resources.Designer.cs
+++ MecyApplication/Properties/Resources.Designer.cs
@@ -373,6 +373,16 @@ namespace MecyApplication.Properties {
         /// <summary>
         ///   Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap ToolbarMeasureDistance {
+            get {
+                object obj = ResourceManager.GetObject("ToolbarMeasureDistance", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap ToolbarMesoId {
             get {
                 object obj = ResourceManager.GetObject("ToolbarMesoId", resourceCulture);
