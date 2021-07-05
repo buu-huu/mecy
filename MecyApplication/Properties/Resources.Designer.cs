818e5b9ceac8f75a7632f058d8186c479f53df5c
buu-huu <luca-schwarz@outlook.de>
2021-07-05 21:31:37 +0200

    Add symbol and change application name in project configuration

--- MecyApplication/Properties/Resources.Designer.cs
+++ MecyApplication/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MecyApplication.Properties {
+namespace Mecy.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MecyApplication.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MecyApplication.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Mecy.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
