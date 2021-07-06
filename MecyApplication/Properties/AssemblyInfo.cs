4b415aee6b71cfa642ba25f67a7d8c76198cc4b4
buu-huu <luca-schwarz@outlook.de>
2021-07-06 10:12:28 +0200

    Change assembly information

--- MecyApplication/Properties/AssemblyInfo.cs
+++ MecyApplication/Properties/AssemblyInfo.cs
@@ -8,10 +8,10 @@ using System.Windows;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("MecyApplication")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Mesocyclone Visualization Software")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MecyApplication")]
+[assembly: AssemblyCompany("Mecy Team")]
+[assembly: AssemblyProduct("Mecy")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
